7a8512c5ab627af325d2ad3f6394514d684391eb
Christoph <christoph.joerimann@stud.hslu.ch>
2016-10-11 20:41:17 +0200

    First contact with a new species 0.0

--- FancyFunnyCar/Form1.cs
+++ FancyFunnyCar/Form1.cs
@@ -15,5 +15,10 @@ namespace FancyFunnyCar
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Hello world!");
+        }
     }
 }
